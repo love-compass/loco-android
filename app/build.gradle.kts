fb1a807920a7509462dc12e2233964e80623f7cc
Yeojun Yoon <yjyoon.dev@gmail.com>
2023-03-28 14:55:53 +0900

    chore: Groovy to KTS & apply Version Catalog with dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    kotlin("android")
+    kotlin("kapt")
+    id("com.android.application")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "dev.yjyoon.locoai"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "dev.yjyoon.locoai"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0.0"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/INDEX.LIST"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.viewmodel.ktx)
+    implementation(libs.androidx.viewmodel.compose)
+
+    implementation(libs.androidx.splashscreen)
+    implementation(libs.androidx.datastore.preferences)
+
+    implementation(libs.compose.ui.core)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material.iconscore)
+    implementation(libs.compose.material.iconsext)
+    implementation(libs.compose.material3)
+
+    implementation(libs.squareup.okhttp)
+    implementation(libs.squareup.okhttp.logging)
+    implementation(libs.squareup.retrofit)
+
+    implementation(libs.coil.compose)
+
+    implementation(libs.google.hilt.android)
+    kapt(libs.google.hilt.compiler)
+
+    debugImplementation(libs.compose.ui.tooling)
+}
