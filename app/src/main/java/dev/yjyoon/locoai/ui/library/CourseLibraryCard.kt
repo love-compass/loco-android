f85f987d41aa3556e89d1daf862d1a1f5ae0d9de
yjyoon-dev <yjyoon.dev@gmail.com>
2023-04-04 02:55:33 +0900

    feat: 데이트 코스 라이브러리 UI 구현

---
+++ app/src/main/java/dev/yjyoon/locoai/ui/library/CourseLibraryCard.kt
@@ -0,0 +1,131 @@
+package dev.yjyoon.locoai.ui.library
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ReceiptLong
+import androidx.compose.material.icons.rounded.CalendarToday
+import androidx.compose.material.icons.rounded.NearMe
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import dev.yjyoon.locoai.R
+import dev.yjyoon.locoai.ui.model.DateCourse
+import java.text.DecimalFormat
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun CourseLibraryCard(
+    dateCourse: DateCourse,
+    onClick: () -> Unit
+) {
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+
+    ElevatedButton(
+        onClick = onClick,
+        shape = RoundedCornerShape(12.dp),
+        contentPadding = PaddingValues(0.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            AsyncImage(
+                model = dateCourse.courses[0].place.imageUrl,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .width(screenWidth / 3)
+                    .aspectRatio(4 / 3f)
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 12.dp, vertical = 8.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = dateCourse.location,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(6.dp))
+                CourseLibraryCardRowTag(
+                    icon = Icons.Rounded.CalendarToday,
+                    text = dateCourse.date.format(
+                        DateTimeFormatter.ofPattern(stringResource(id = R.string.date_with_year_fmt))
+                    )
+                )
+                Spacer(modifier = Modifier.height(3.dp))
+                CourseLibraryCardRowTag(
+                    icon = Icons.Rounded.NearMe,
+                    text = stringResource(
+                        id = R.string.visited_place_count,
+                        dateCourse.courses.size
+                    )
+                )
+                Spacer(modifier = Modifier.height(3.dp))
+                CourseLibraryCardRowTag(
+                    icon = Icons.Filled.ReceiptLong,
+                    text = dateCourse.totalBudget.let {
+                        if (it == 0) {
+                            stringResource(id = R.string.free)
+                        } else {
+                            stringResource(
+                                id = R.string.total_budget,
+                                DecimalFormat("#,###").format(it)
+                            )
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CourseLibraryCardRowTag(
+    icon: ImageVector,
+    text: String,
+) {
+    val color = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.6f)
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = color,
+            modifier = Modifier.size(14.dp)
+        )
+        Text(
+            text = text,
+            fontSize = 12.sp,
+            color = color,
+            modifier = Modifier.padding(horizontal = 12.dp)
+        )
+    }
+}
--- app/src/main/java/dev/yjyoon/locoai/ui/library/CourseLibraryScreen.kt
+++ app/src/main/java/dev/yjyoon/locoai/ui/library/CourseLibraryScreen.kt
@@ -1,8 +1,34 @@
 package dev.yjyoon.locoai.ui.library
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.yjyoon.locoai.R
+import dev.yjyoon.locoai.ui.input.QuitButton
 
 @Composable
 fun CourseLibraryScreen(
@@ -10,4 +36,55 @@ fun CourseLibraryScreen(
     onClose: () -> Unit
 ) {
     val state by viewModel.uiState.collectAsState()
+    val listState = rememberLazyListState()
+
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    enabled = false,
+                    onClick = { },
+                    modifier = Modifier.padding(vertical = 12.dp)
+                ) {
+                    Icon(
+                        Icons.Filled.Close,
+                        contentDescription = null,
+                        modifier = Modifier.alpha(0f)
+                    )
+                }
+                Text(
+                    text = stringResource(id = R.string.course_view),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                QuitButton(onClick = onClose)
+            }
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                state = listState
+            ) {
+                items(items = state.dateCourses) {
+                    CourseLibraryCard(
+                        dateCourse = it,
+                        onClick = { }
+                    )
+                }
+                item {
+                    Spacer(modifier = Modifier.height(12.dp))
+                }
+            }
+        }
+    }
 }
