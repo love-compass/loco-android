ce0ca598069807633bf8d1b3fa9f471c3e9ef8a9
yjyoon-dev <yjyoon.dev@gmail.com>
2023-04-04 21:28:53 +0900

    chore: 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.yjyoon.locoai"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         vectorDrawables {
             useSupportLibrary = true
         }
